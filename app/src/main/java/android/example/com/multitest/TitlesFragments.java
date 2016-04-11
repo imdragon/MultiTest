602e7ab20051768865097d6a888cbdc3a1085b0f
imdragon <i.m.dragon@gmail.com>
2016-04-11 13:23:37 -0700

    Fixed some issues, now intent launches

--- app/src/main/java/android/example/com/multitest/TitlesFragments.java
+++ app/src/main/java/android/example/com/multitest/TitlesFragments.java
@@ -1,6 +1,5 @@
 package android.example.com.multitest;
 
-import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.app.ListFragment;
 import android.content.Intent;
@@ -9,92 +8,121 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-/**
- * Created by Admin on 4/7/2016.
- */
+// Shows the title fragment which is a ListView
+// When a ListView item is selected we will put the DetailsFragment in the
+// Framelayout if we are in horizontal mode, or we will create a DetailsActivity // and switch to it if in portrait mode
 public class TitlesFragments extends ListFragment {
 
-    boolean mDualPane;
+    // True or False depending on if we are in horizontal or duel pane mode
+    boolean mDuelPane;
+
+    // Currently selected item in the ListView
     int mCurCheckPosition = 0;
 
-    /**
-     * Called when the fragment's activity has been created and this
-     * fragment's view hierarchy instantiated.  It can be used to do final
-     * initialization once these pieces are in place, such as retrieving
-     * views or restoring state.  It is also useful for fragments that use
-     * {@link #setRetainInstance(boolean)} to retain their instance,
-     * as this callback tells the fragment when it is fully associated with
-     * the new activity instance.  This is called after {@link #onCreateView}
-     * and before {@link #onViewStateRestored(Bundle)}.
-     *
-     * @param savedInstanceState If the fragment is being re-created from
-     *                           a previous saved state, this is the state.
-     */
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        ArrayAdapter<String> connectArrayToListView = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_activated_1, SuperHeroInfo.NAMES);
+        // An ArrayAdapter connects the array to our ListView
+        // getActivity() returns a Context so we have the resources needed
+        // We pass a default list item text view to put the data in and the
+        // array
+        ArrayAdapter<String> connectArrayToListView = new ArrayAdapter<String>(
+                getActivity(),
+                android.R.layout.simple_list_item_activated_1,
+                SuperHeroInfo.NAMES);
 
+        // Connect the ListView to our data
         setListAdapter(connectArrayToListView);
 
+        // Check if the FrameLayout with the id details exists
         View detailsFrame = getActivity().findViewById(R.id.details);
 
-        mDualPane = detailsFrame != null && detailsFrame.getVisibility() == View.VISIBLE;
+        // Set mDuelPane based on whether you are in the horizontal layout
+        // Check if the detailsFrame exists and if it is visible
+        mDuelPane = detailsFrame != null && detailsFrame.getVisibility() == View.VISIBLE;
 
-        if(savedInstanceState != null){
+        // If the screen is rotated onSaveInstanceState() below will store the // hero most recently selected. Get the value attached to curChoice and // store it in mCurCheckPosition
+        if (savedInstanceState != null) {
+            // Restore last state for checked position.
             mCurCheckPosition = savedInstanceState.getInt("curChoice", 0);
+        }
 
-            if (mDualPane){
-                getListView().setChoiceMode(ListView.CHOICE_MODE_SINGLE);
-                showDetails(mCurCheckPosition);
-            }
+        if (mDuelPane) {
+            // CHOICE_MODE_SINGLE allows one item in the ListView to be selected at a time
+            // CHOICE_MODE_MULTIPLE allows multiple
+            // CHOICE_MODE_NONE is the default and the item won't be highlighted in this case'
+            getListView().setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+
+            // Send the item selected to showDetails so the right hero info is shown
+            showDetails(mCurCheckPosition);
         }
     }
 
-
+    // Called every time the screen orientation changes or Android kills an Activity
+    // to conserve resources
+    // We save the last item selected in the list here and attach it to the key curChoice
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putInt("curChoice", mCurCheckPosition);
     }
 
-    /**
-     * This method will be called when an item in the list is selected.
-     * Subclasses should override. Subclasses can call
-     * getListView().getItemAtPosition(position) if they need to access the
-     * data associated with the selected item.
-     *
-     * @param l        The ListView where the click happened
-     * @param v        The view that was clicked within the ListView
-     * @param position The position of the view in the list
-     * @param id       The row id of the item that was clicked
-     */
+    // When a list item is clicked we want to change the hero info
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
         showDetails(position);
     }
 
-    private void showDetails(int index) {
+    // Shows the hero data
+    void showDetails(int index) {
+
+        // The most recently selected hero in the ListView is sent
         mCurCheckPosition = index;
-        if(mDualPane){
+
+        // Check if we are in horizontal mode and if yes show the ListView and
+        // the hero data
+        if (mDuelPane) {
+
+            // Make the currently selected item highlighted
             getListView().setItemChecked(index, true);
 
-            DetailsFragment details = (DetailsFragment) getFragmentManager().findFragmentById(R.id.details);
+            // Create an object that represents the current FrameLayout that we will
+            // put the hero data in
+            DetailsFragment details = (DetailsFragment)
+                    getFragmentManager().findFragmentById(R.id.details);
 
-            if(details == null || details.getShownIndex() != index){
-details = DetailsFragment.newInstance(index);
+            // When a DetailsFragment is created by calling newInstance the index for the data
+            // it is supposed to show is passed to it. If that index hasn't been assigned we must
+            // assign it in the if block
+            if (details == null || details.getShownIndex() != index) {
+
+                // Make the details fragment and give it the currently selected hero index
+                details = DetailsFragment.newInstance(index);
+
+                // Start Fragment transactions
                 FragmentTransaction ft = getFragmentManager().beginTransaction();
+
+                // Replace any other Fragment with our new Details Fragment with the right data
                 ft.replace(R.id.details, details);
+
+                // TRANSIT_FRAGMENT_FADE calls for the Fragment to fade away
                 ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
                 ft.commit();
-            }else {
-                Intent intent = new Intent();
-                intent.setClass(getActivity(), DetailsActivity.class);
-                intent.putExtra("index", index);
-                startActivity(intent);
             }
-        }
 
+        } else {
+            // Launch a new Activity to show our DetailsFragment
+            Intent intent = new Intent();
+
+            // Define the class Activity to call
+            intent.setClass(getActivity(), DetailsActivity.class);
+
+            // Pass along the currently selected index assigned to the keyword index
+            intent.putExtra("index", index);
+
+            // Call for the Activity to open
+            startActivity(intent);
+        }
     }
 }
